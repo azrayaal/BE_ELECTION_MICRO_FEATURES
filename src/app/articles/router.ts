f657e10cf09156e7da38ddee6a7bc8bd4debb374
azra <azrayazidalkautsar@gmail.com>
2024-01-03 08:41:22 +0700

    update auth

--- src/app/articles/router.ts
+++ src/app/articles/router.ts
@@ -5,7 +5,8 @@ import AuthMiddleware from "../../middlewares/auth"
 
 const router = express.Router()
 
-router.post('/article',  uploadFile.upload("image"), ArticleControllers.create)
+// router.post('/article',  uploadFile.upload("image"), ArticleControllers.create)
+router.post('/article',  AuthMiddleware.Auth, uploadFile.upload("image"), ArticleControllers.create)
 router.get('/article',  AuthMiddleware.Auth, ArticleControllers.find)
 router.get('/article/:id', ArticleControllers.getDetail)
 
